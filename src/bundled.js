74ea0355264d4536d81a321195c92714a61e3a8c
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-04-08 02:18:27 +0200

    Build process belen bolj Online bolhod belen bolloo

---
+++ src/bundled.js
@@ -0,0 +1,5 @@
+import "../src/styles/styles.css";
+import toggle from "./scripts/toggle_menu";
+import toTop from "./scripts/toTop";
+import slide from "./scripts/library/home_page";
+import contact from "./scripts/contact";
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import "../src/styles/styles.css";
-import toggle from "../src/scripts/toggle_menu";
-import toTop from "../src/scripts/toTop";
-import slide from "../src/scripts/library/home_page";
-import contact from "../src/scripts/contact";
