56634575789e9092db7414d2dd1d007eeb3a0a17
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-04-13 08:15:13 +0200

    Video_page deer SwiperSlider nemlee

--- src/bundled.js
+++ src/bundled.js
@@ -2,3 +2,5 @@ import "../src/styles/styles.css";
 import toggle from "./scripts/toggle_menu";
 import toTop from "./scripts/toTop";
 import contactPageEvent from "./scripts/contact";
+import swiperSlider from "./scripts/swiper-slider";
+import venobox from "./scripts/venobox";
---
+++ src/scripts/venobox.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".venobox").venobox({
+    closeColor: "#fab733",
+    arrowsColor: "#fab733",
+    spinColor: "#fab733",
+    spinner: "cube-grid",
+  });
+});
