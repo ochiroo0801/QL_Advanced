cbb05453e8d592d58ffda979c39fc5a3245e195f
ochiroo.beatz <59750754+ochiroo0121@users.noreply.github.com>
2020-05-06 16:13:36 +0200

    Form Hesgiig hiij duuslaa

--- src/scripts/library/_form.js
+++
@@ -1,8 +0,0 @@
-$("#my-form").submit(function (e) {
-  e.preventDefault();
-
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function () {
-    alert("Thank you!");
-  });
-});
